1d6528ba7e32c4df4f5c5cfc72326bfe3d1c8cb3
Matthew Schranz <schranz.m@gmail.com>
2012-03-03 17:46:59 -0500

    A wild dead link appears! Removed an old link from AgreementForm.jsp

--- PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Company_.java
+++ PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Company_.java
@@ -4,12 +4,14 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-02-28T23:29:57")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-03-03T17:44:49")
 @StaticMetamodel(Company.class)
 public class Company_ { 
 
+    public static volatile SingularAttribute<Company, String> companyDescription;
     public static volatile SingularAttribute<Company, String> companyPhone;
     public static volatile SingularAttribute<Company, Integer> userId;
+    public static volatile SingularAttribute<Company, String> businessAreas;
     public static volatile SingularAttribute<Company, Integer> companyId;
     public static volatile SingularAttribute<Company, String> companyName;
 
