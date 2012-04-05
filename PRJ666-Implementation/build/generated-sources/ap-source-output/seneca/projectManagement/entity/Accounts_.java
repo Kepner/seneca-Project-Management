83656d465a03a4304ead6ee39d8b8cd4baaf8d55
Matthew Schranz <schranz.m@gmail.com>
2012-04-05 10:50:29 -0400

    Site now runs on zenit. Fixed up persistence XML and links on Agreement Page

--- PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Accounts_.java
+++ PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Accounts_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-04-01T14:54:46")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-04-05T10:21:58")
 @StaticMetamodel(Accounts.class)
 public class Accounts_ { 
 
