3ada3b3467abe5051d6d469486abb90ba8eeb4c0
Matthew Schranz <schranz.m@gmail.com>
2012-04-09 21:48:36 -0400

    Final Changes

--- PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Teams_.java
+++ PRJ666-Implementation/build/generated-sources/ap-source-output/seneca/projectManagement/entity/Teams_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-04-05T23:07:57")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-04-09T21:05:44")
 @StaticMetamodel(Teams.class)
 public class Teams_ { 
 
